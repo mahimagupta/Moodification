79d927b343922dea02fed420f5cdfd8100ed9ee2
Mahima Gupta <mahimagupta2795@gmail.com>
2018-08-09 16:26:01 -0700

    Updated

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -33,7 +33,7 @@ var handlers = {
   },
   'SadIntent' : function() {
     
-         //flag=1;
+         flag=1;
          //this.emit('PlayStream');
          streamInfo.url1=randomPhrase(Happy);
          this.response.speak('Let me play you a Happy Song.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
@@ -42,27 +42,31 @@ var handlers = {
   },
   'LazyIntent' : function() {
          
+         flag=2;
          streamInfo.url1=randomPhrase(Energetic);
          this.response.speak('Lets change your mood.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
          this.emit(':responseReady');
       
   },
   'RomanticIntent' : function() {
-    
+         
+         flag=3;
          streamInfo.url1=randomPhrase(Romantic);
          this.response.speak('Let me play a romantic song.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
          this.emit(':responseReady');
       
   },
   'PartyIntent' : function() {
-    
+         
+         flag=4;
          streamInfo.url1=randomPhrase(Party);
          this.response.speak('Let me play some party song.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
          this.emit(':responseReady');
       
   },
   'CalmIntent' : function() {
-    
+         
+         flag=5;
          streamInfo.url1=randomPhrase(Relax);
          this.response.speak('Let me play some soothing music.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
          this.emit(':responseReady');
@@ -72,22 +76,7 @@ var handlers = {
     this.emit(':ask','khatam');
   },
   
-  // 'PlayStream': function() {
-  //   if(flag==1)
-  //     {
-  //        streamInfo.url1=randomPhrase(Sad);
-  //        this.response.speak('Lets change your mood.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
-  //        this.emit(':responseReady');
 
-  //     }
-  //   else
-  //     {
-  //       streamInfo.url2=randomPhrase(Happy);
-  //       this.response.speak('Let me play some music.').audioPlayerPlay('REPLACE_ALL', streamInfo.url2, streamInfo.url2, null, 0);
-  //       this.emit(':responseReady');
-  //     }
-    
-  // },
   'AMAZON.HelpIntent': function() {
     // skill help logic goes here
     this.emit(':responseReady');
@@ -107,8 +96,38 @@ var handlers = {
     this.emit(':responseReady');
    },
   'AMAZON.NextIntent': function() {
-    this.response.speak('This skill does not support skipping.');
+  
+  if(flag==1)
+  {  
+    streamInfo.url1=randomPhrase(Happy2);
+    this.response.speak('Let me play another Happy Song.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
     this.emit(':responseReady');
+  }
+  else if (flag==5)
+  {
+     streamInfo.url1=randomPhrase(Relax2);
+    this.response.speak('Let me play another relaxing Song.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
+    this.emit(':responseReady');
+  }
+  else if (flag==2)
+  {
+    streamInfo.url1=randomPhrase(Energetic2);
+    this.response.speak('Let me play another energetic Song.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
+    this.emit(':responseReady');
+  }
+    else if (flag==3)
+  {
+    streamInfo.url1=randomPhrase(Romantic2);
+    this.response.speak('Let me play another romantic Song.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
+    this.emit(':responseReady');
+  }
+  else
+  {
+    streamInfo.url1=randomPhrase(Party2);
+    this.response.speak('Let me play another party Song.').audioPlayerPlay('REPLACE_ALL', streamInfo.url1, streamInfo.url1, null, 0);
+    this.emit(':responseReady');
+  }
+
   },
   'AMAZON.PreviousIntent': function() {
     this.response.speak('This skill does not support skipping.');
@@ -121,7 +140,7 @@ var handlers = {
     this.emit('AMAZON.StopIntent');
   },
   'AMAZON.StopIntent': function() {
-    this.response.speak('Okay. I\'ve stopped the stream.').audioPlayerStop();
+    this.response.speak('Okay. GoodBye! See you later.').audioPlayerStop();
     this.emit(':responseReady');
   },
   'AMAZON.ResumeIntent': function() {
@@ -165,10 +184,19 @@ const Party=['https://s3.amazonaws.com/rocksong/rockstar.mp3','https://s3.amazon
 
 const Relax=['https://s3.amazonaws.com/rocksong/Beyonce%CC%81+-+Halo.mp3','https://s3.amazonaws.com/rocksong/Photograph+-+Ed+Sheeran+(Lyrics).mp3','https://s3.amazonaws.com/rocksong/Vance_Joy_-_Riptide_(mp3.pm).mp3'];
 
+const Romantic2=['https://s3.amazonaws.com/rocksong/Niall+Horan+-+This+Town+(Lyric+Video).mp3','https://s3.amazonaws.com/rocksong/The+Chainsmokers+-+Closer+(Lyrics)(ft.+Halsey).mp3','https://s3.amazonaws.com/rocksong/She+Will+Be+Loved-+Maroon+5+%5B%5Bwith+lyrics%5D%5D.mp3'];
+
+const Energetic2=['https://s3.amazonaws.com/rocksong/Beastie+Boys+-+Body+Movin\'.mp3','https://s3.amazonaws.com/rocksong/Salt-N-Pepa+-+Push+It.mp3','https://s3.amazonaws.com/rocksong/Shake+Your+Body+(Down+to+the+Ground).mp3'];
+
+const Happy2=['https://s3.amazonaws.com/rocksong/Maroon+5+-+Girls+Like+You+ft.+Cardi+B.mp3', 'https://s3.amazonaws.com/rocksong/The+Chainsmokers+-+Paris+(Lyric).mp3', 'https://s3.amazonaws.com/rocksong/American+Authors+-+Best+Day+Of+My+Life.mp3'];
+
+const Party2=['https://s3.amazonaws.com/rocksong/Sia+-+Cheap+Thrills+(Lyric+Video)+ft.+Sean+Paul.mp3','https://s3.amazonaws.com/rocksong/Paramore+Ain\'t+It+Fun+%5BOFFICIAL+VIDEO%5D.mp3', 'https://s3.amazonaws.com/rocksong/Walk+The+Moon+Shut+Up+and+Dance+With+Me+Lyrics.mp3'];
+
+const Relax2=['https://s3.amazonaws.com/rocksong/All+Saints+-+Pure+Shores+(Official+Music+Video).mp3','https://s3.amazonaws.com/rocksong/Coldplay+-+The+Scientist.mp3','https://s3.amazonaws.com/rocksong/Coldplay+-+Paradise+(Official+Video).mp3',];
+
 
 
 function randomPhrase(myData) {
-    // the argument is an array [] of words or phrases
 
     var i = 0;
 
@@ -189,8 +217,6 @@ var audioEventHandlers = {
     this.emit(':responseReady');
   },
   'PlaybackNearlyFinished': function() {
-    //this.response.audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
-    //this.emit(':responseReady');
     this.emit(':ask', 'Do you have any other questions for me?');
   },
   'PlaybackFailed': function() {
